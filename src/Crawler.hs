79cd7da9456e513ba78434219132482cae9ec96d
pastchick3 <pastchick3@gmail.com>
2021-12-29 18:51:58 +0800

    init dev

--- src/Crawler.hs
+++ src/Crawler.hs
@@ -2,99 +2,90 @@
 
 module Crawler (main) where
 
-import System.Environment (getArgs)
-import qualified Data.ByteString.Lazy as LBS
-
-import Network.HTTP.Simple (Response, HttpException, parseRequest, getResponseBody, httpLBS, setRequestHeader, setRequestHeaders, getResponseHeader)
-import Data.Traversable (forM)
-
-import           Control.Exception          (try)
-
-import Text.HTML.TagSoup
+import Control.Exception (try)
+import qualified Data.Aeson as Aeson(Key, Value (Number, Object, String), decode)
+import qualified Data.Aeson.Key as Key (fromString)
+import qualified Data.Aeson.KeyMap as Aeson (toMap)
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as LBS (writeFile)
 import qualified Data.ByteString.Lazy.UTF8 as LBU
+import qualified Data.Map as Map ((!))
+import qualified Data.Scientific as Scientific (toBoundedInteger)
+import Data.Text (Text, pack, unpack)
+import Network.HTTP.Simple (HttpException, Response, getResponseBody, httpLBS, parseRequest, setRequestHeaders)
+import System.Directory (createDirectory)
+import System.Environment (getArgs)
+import Text.HTML.TagSoup (fromAttrib, parseTags, (~/=))
+import Text.Regex.TDFA ((=~))
 
-import Data.Aeson
-import Data.Aeson.KeyMap as K
-import qualified Data.Vector as V
-import qualified Data.Map as M
-import qualified Data.Text as T
-import Data.Text (Text)
+retry :: Int
+retry = 1
 
-import Text.Regex.TDFA
--- import Data.ByteString (ByteString)
+path = "/mnt/c/Users/33160/Desktop/"
 
-userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
-            \Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62"
-referer = "https://www.pixiv.net/"
-artworksBase = "https://www.pixiv.net/artworks/"
-path = "/mnt/c/Users/33160/Desktop"
+userAgent =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+  \Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62"
 
-requestUrl :: String -> Int -> IO (Either HttpException (Response LBS.ByteString))
-requestUrl url retry = do
-    request' <- parseRequest url
-    let request = setRequestHeaders [("User-Agent", userAgent), ("Referer", referer)] $ request'
-    eresponse <- try $ httpLBS request
-    if retry == 0 then return eresponse else
-        case eresponse :: Either HttpException (Response LBS.ByteString) of
-            Left e -> requestUrl url $ retry - 1
-            Right response -> return (Right response)
+data Artwork = Artwork {artist :: String, date :: String, title :: String, contentId :: String}
 
-data Artwork = Artwork {artist::Text, date::Text, title::Text, id::Text}
-
-extractField :: Value -> [Key] -> Text
-extractField (Object obj) fields =   do 
-    let m = K.toMap  obj
-    case fields of
-        [x] -> case m M.! x of
-            String s -> s
-        x:xs -> case m M.! x of
-                o -> extractField o xs
-
-
+instance Show Artwork where
+  show (Artwork artist date title contentId) = "[" ++ artist ++ "] [" ++ date ++ "] " ++ title ++ " (" ++ contentId ++ ")"
 
 main :: IO ()
-main = do
-    args <- getArgs
-    let id = args !! 0
-    
-    eresponse <- requestUrl  (artworksBase ++ id ) 1
+main = getArgs >>= mapM_ crawlArtwork
 
-    case eresponse of
-        Left e -> print (e :: HttpException)
-        Right response -> do
-            print $ getResponseHeader "Content-Type" response
-            LBS.writeFile (path++"/t.html") $ getResponseBody response
+crawlArtwork :: String -> IO ()
+crawlArtwork contentId = do
+  putStrLn ("Crawl " ++ contentId)
+  response <- requestUrl ("https://www.pixiv.net/artworks/" ++ contentId) retry
+  case response of
+    Left _ -> print ("    Fail to crawl " ++ contentId)
+    Right response -> do
+      let (artwork, imgBase, imgExt, pageCount) = extractArtwork contentId response
+      createDirectory $ path ++ show artwork
+      mapM_ (crawlImage contentId artwork imgBase imgExt) [0 .. pageCount - 1]
+      print "done"
 
-            let src = LBU.toString $ getResponseBody response
-            let content = fromAttrib "content"  $ head $ extractContent $ parseTags src
-                
+requestUrl :: String -> Int -> IO (Either HttpException (Response ByteString))
+requestUrl url retry = do
+  request <- parseRequest url
+  let headers = [("User-Agent", userAgent), ("Referer", "https://www.pixiv.net/")]
+  response <- try $ httpLBS $ setRequestHeaders headers request
+  case (retry, response) of
+    (retry, Left _) | retry > 0 -> requestUrl url (retry - 1)
+    (_, response) -> return response
 
-            let json = (decode $ LBU.fromString content :: Maybe Value)
-            case json of
-                Nothing -> error "no json"
-                Just v -> print m
-                    where
-                        t = extractField v ["illust", "94982825", "createDate"]
-                        reg = "^[0-9]{2}([0-9]+)-([0-9]+)-([0-9]+)" :: T.Text
-                        m = (t =~ reg :: (T.Text, T.Text, T.Text, [T.Text]))
-            print "done"
-            where
-                    s = "<meta id=meta-preload-data>"::String
-                    extractContent = take 1 . dropWhile (~/= s)
-            
+extractArtwork :: String -> Response ByteString -> (Artwork, String, String, Int)
+extractArtwork contentId response = (artwork, unpack imgBase, unpack imgExt, pageCount)
+  where
+    extractContent = take 1 . dropWhile (~/= ("<meta id=meta-preload-data>"::String))
+    content = fromAttrib "content" $ head $ extractContent $ parseTags $ getResponseBody response
+    Just json = Aeson.decode content
+    obj = json ! "illust" ! Key.fromString contentId
+    Aeson.String artist = obj ! "userName"
+    Aeson.String rawDate = obj ! "createDate"
+    dateRegex = "^[0-9]{2}([0-9]{2})-([0-9]{2})-([0-9]{2})" :: Text
+    (_, _, _, [year, month, day]) = rawDate =~ dateRegex:: (Text,Text,Text,[Text])
+    date = year <> month <> day
+    Aeson.String title = obj ! "title"
+    artwork = Artwork {artist = unpack artist, date = unpack date, title = unpack title, contentId = contentId}
+    Aeson.Number rawPageCount = obj ! "pageCount"
+    Just pageCount = Scientific.toBoundedInteger rawPageCount
+    Aeson.String rawUrl = obj ! "urls" ! "original"
+    urlRegex = "[0-9]+(\\.[a-z]+)$" ::String
+    (imgBase, _, _, [imgExt]) = rawUrl =~ urlRegex :: (Text,Text,Text,[Text])
 
-            
-          
+(!) :: Aeson.Value -> Aeson.Key -> Aeson.Value
+Aeson.Object obj ! field = do
+  let map = Aeson.toMap obj
+  map Map.! field
 
-
-    -- let picUrl = "https://i.pximg.net/img-original/img/2021/12/24/20/00/03/94982825_p0.jpg"
-
-    -- request' <- parseRequest picUrl
-    -- let request'' = setRequestHeader "User-Agent" [userAgent] request'
-    -- let request = setRequestHeader "Referer" ["https://www.pixiv.net/"] request''
-    -- response <- httpLBS request
-    -- putStrLn $ "The status code was: " ++ show (getResponseStatusCode response)
-    -- print $ getResponseHeader "Content-Type" response
-    -- L8.writeFile (path++"/t.jpg") $ getResponseBody response
-    -- print "done"
-    
+crawlImage :: String -> Artwork -> String -> String -> Int -> IO ()
+crawlImage contentId artwork imgBase imgExt pageCount = do
+    let url = imgBase ++ show pageCount ++ imgExt
+    response <- requestUrl url retry
+    let path = path ++ show artwork ++ "/" ++ contentId ++ "_p" ++ show pageCount ++ imgExt
+    case response of
+        Left _ -> putStrLn ("    Fail to crawl P" ++ show pageCount)
+        Right response -> LBS.writeFile path $getResponseBody response
